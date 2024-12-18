4afd42c72216dc5d39c851eb3bdb02e0bbbb71ae
tsikot39 <corpusjohnbenedict@gmail.com>
2024-12-18 09:28:24 -0700

    first commit

---
+++ src/components/Works.jsx
@@ -0,0 +1,150 @@
+"use client";
+
+import React, { useState } from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}) => {
+  return (
+    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
+
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+const Works = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 3;
+
+  // Calculate the indices for the current page
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
+
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} `}>My work</p>
+        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+      </motion.div>
+
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          The following projects showcase my skills and expertise through
+          practical applications of software development. Each project is
+          described briefly, with links to code repositories or site URLs,
+          highlighting my ability to deliver effective solutions, leverage
+          various technologies, and manage projects effectively.
+        </motion.p>
+      </div>
+
+      <motion.div
+        key={currentPage}
+        initial="hidden"
+        animate="show"
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-20 flex flex-wrap gap-7"
+      >
+        {currentProjects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </motion.div>
+
+      <div className="mt-10 flex justify-center gap-4">
+        <button
+          onClick={handlePrevPage}
+          disabled={currentPage === 1}
+          className="w-36 bg-tertiary py-2 px-4 rounded-xl outline-none text-white font-bold shadow-md shadow-primary disabled:opacity-50"
+        >
+          &larr; Previous
+        </button>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === totalPages}
+          className="w-36 bg-tertiary py-2 px-4 rounded-xl outline-none text-white font-bold shadow-md shadow-primary disabled:opacity-50"
+        >
+          Next &rarr;
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(Works, "projects");
---
+++ src/constants/index.js
@@ -0,0 +1,692 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  threejs,
+  storeit,
+  carepulse,
+  livedocs,
+  ycdirectory,
+  bowcourse,
+  inventory,
+  forkify,
+  omnifood,
+  tasktool,
+  pms,
+  dpa,
+  ord,
+  ris,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "projects",
+    title: "Projects",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Next.js/React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Desktop Application Developer",
+    icon: backend,
+  },
+  {
+    title: "C# Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "C#",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "MS SQL",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "Next JS",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Senior Computer Programmer",
+    company_name: "Philippine Children's Medical Center",
+    icon: starbucks,
+    iconBg: "#383E56",
+    date: "November 2006 - July 2023",
+    points: [
+      "Developed and maintained web and desktop applications, prioritizing high performance and user experience.",
+      "Led the implementation and deployment of a hospital-wide information system, enhancing operational efficiency.",
+      "Led technical teams, overseeing project deliverables, and ensuring timely completion.",
+      "Conducted comprehensive training sessions, elevating the technological proficiency of users.",
+    ],
+  },
+  {
+    title: "System Administrator",
+    company_name: "Woodfields Consultants Inc.",
+    icon: tesla,
+    iconBg: "#E6DEDD",
+    date: "July 2001 - September 2006",
+    points: [
+      "Managed and optimized network infrastructure, ensuring reliability and performance.",
+      "Set up and configured computing devices, aligning with user needs and organizational standards.",
+      "Implemented system upgrades and maintenance, securing the IT in infrastructure.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "StoreIt",
+    description:
+      "An intuitive platform for effortless file storage, organization, and sharing, built with Next.js 15 and powered by the Appwrite Node SDK for seamless management..",
+    tags: [
+      {
+        name: "next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "shadcn",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "chart.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "appwrite",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: storeit,
+    source_code_link: "https://jbc-store-it.vercel.app/",
+  },
+  {
+    name: "Care Pulse",
+    description:
+      "A patient management app for easy registration, booking, and appointment management, featuring admin tools and SMS notifications, built on Next.js.",
+    tags: [
+      {
+        name: "next.js",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "appwrite",
+        color: "green-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "shadcn",
+        color: "green-text-gradient",
+      },
+      {
+        name: "twilio",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carepulse,
+    source_code_link: "https://jbc-carepulse.vercel.app/",
+  },
+  {
+    name: "Live Docs",
+    description:
+      "A Google Docs clone, uses Liveblocks for real-time collaboration, TailwindCSS for styling, and Next.js to showcase expertise in impactful real-time applications.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+      {
+        name: "lexicaleditor",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "shadcn",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "clerk",
+        color: "green-text-gradient",
+      },
+      {
+        name: "liveblocks",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: livedocs,
+    source_code_link: "https://jbc-livedocs.vercel.app/",
+  },
+  {
+    name: "YC Directory",
+    description:
+      "A Next.js 15 platform for entrepreneurs to pitch ideas, join competitions, and explore others with a sleek, user-friendly design.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "sanity",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwindcss",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "shadcn",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "typescript",
+        color: "green-text-gradient",
+      },
+    ],
+    image: ycdirectory,
+    source_code_link: "https://jbc-ycdirectory.vercel.app/",
+  },
+  {
+    name: "Bow Course Registration System",
+    description:
+      "A MERN stack platform where students register for courses, view schedules, and admins efficiently manage programs and enrollments.",
+    tags: [
+      {
+        name: "mongodb",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "express.js",
+        color: "green-text-gradient",
+      },
+      {
+        name: "react.js",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "node.js",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: bowcourse,
+    source_code_link: "https://github.com/tsikot39/bow-course",
+  },
+  {
+    name: "Inventory Management System",
+    description:
+      "Helps store managers efficiently track products, monitor stock levels, and generate reports for streamlined inventory control.",
+    tags: [
+      {
+        name: "csharp",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "windowsforms",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: inventory,
+    source_code_link: "https://github.com/tsikot39/rad_finalproject_group5",
+  },
+  {
+    name: "Task Management Tool",
+    description:
+      "A windows forms application for managing tasks, featuring add, edit, delete, and mark as completed options, with status filtering, title search, and color-coded due date tracking.",
+    tags: [
+      {
+        name: "csharp",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "windowsforms",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: tasktool,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Forkify",
+    description:
+      "A web app to search over a million recipes, bookmark favorites, and add personal recipes, built with HTML, SASS, JavaScript, Parcel, and API integration.",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "sass",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "api",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: forkify,
+    source_code_link: "https://jbc-forkify.vercel.app/",
+  },
+  {
+    name: "Omnifood",
+    description:
+      "A responsive, elegantly designed website offering a personalized daily meal delivery service crafted using JavaScript, HTML and CSS.",
+    tags: [
+      {
+        name: "javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "html",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: omnifood,
+    source_code_link: "https://jbc-omnifood.vercel.app/",
+  },
+  {
+    name: "Patient Information System",
+    description:
+      "A desktop application that organizes and stores detailed patient records, including medical history, demographics, treatments, diagnoses, prescriptions, and other healthcare-related data.",
+    tags: [
+      {
+        name: "vb.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "windowsforms",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: pms,
+    source_code_link: "https://github.com/tsikot39/patient-information-system",
+  },
+  {
+    name: "Doctors PhilHealth Accreditation System",
+    description:
+      "A web application used to monitor and manage the accreditation status of doctors or healthcare providers with PhilHealth, the national health insurance program in the Philippines.",
+    tags: [
+      {
+        name: "asp.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: dpa,
+    source_code_link:
+      "https://github.com/tsikot39/doctors-philhealth-accreditation",
+  },
+  {
+    name: "Online Research Abstract",
+    description:
+      "A web application providing a concise summary of research papers, articles, or studies, highlighting key points, findings, and conclusions for quick understanding.",
+    tags: [
+      {
+        name: "asp.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ord,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Radiology Imaging System",
+    description:
+      "A web application for radiology departments to capture, store, process, and analyze medical images, aiding in diagnostic imaging and medical diagnosis.",
+    tags: [
+      {
+        name: "asp.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "PhilHealth SPC System",
+    description:
+      "A web application to track and manage the period of hospital confinement for insured individuals.",
+    tags: [
+      {
+        name: "asp.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "BLS Courses System",
+    description:
+      "A web application designed to track, manage, and monitor the training and certification of individuals in basic life support courses.",
+    tags: [
+      {
+        name: "asp.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "PhilHealth eClaims Web Service",
+    description:
+      "A windows forms application integrated with PhilHealth eClaims web service for processing claims and reimbursements. Users can check member eligibility, submit claims online, and track their status.",
+    tags: [
+      {
+        name: "vb.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Employee Information System",
+    description:
+      "A desktop application for securely managing and organizing employee information, serving as a centralized hub for data storage and retrieval.",
+    tags: [
+      {
+        name: "vb.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "HR Pooling System",
+    description:
+      "A desktop application for effective human resource management, providing a structured framework for talent acquisition, development, and deployment to align with organizational goals and growth.",
+    tags: [
+      {
+        name: "vb.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Employee Performance Management System",
+    description:
+      "A web application used to assess, track, and manage the performance of employees. This system helps in evaluating individual performance against set goals, identifying areas for improvement, and facilitating constructive feedback.",
+    tags: [
+      {
+        name: "vb.net",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mssql",
+        color: "green-text-gradient",
+      },
+      {
+        name: "ado.net",
+        color: "pink-text-gradient",
+      },
+      {
+        name: ".netframework",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: ris,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
