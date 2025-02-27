a88ac476eee1fb45b8fc0891c8c4657477c98755
tsikot39 <corpusjohnbenedict@gmail.com>
2025-02-27 13:03:03 -0700

    added microsoft in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -55,7 +55,8 @@ const About = () => {
         healthcare information systems, database management, and business logic
         implementation. I hold a B.Sc. in Computer Science and am currently
         pursuing a Software Development Diploma at Bow Valley College, expected
-        to graduate in April 2025. With IBM and Meta Professional Certificates.
+        to graduate in April 2025. With IBM, Meta and Microsoft Professional
+        Certificates.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
