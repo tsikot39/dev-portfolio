e84e978421204fad1d01d2ab857a743503076a0c
tsikot39 <corpusjohnbenedict@gmail.com>
2025-05-10 09:54:52 -0600

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-medium cursor-pointer flex ">
-            Johnson&nbsp;
-            <span className="sm:block hidden">| Portfolio</span>
+            Dev&nbsp;Portfolio
+            {/* <span className="sm:block hidden">| Portfolio</span> */}
           </p>
         </Link>
 
